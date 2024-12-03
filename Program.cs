dccb2a8d53007877d0b6c8d50a2321ebb692b47f
Zhi Wu <zhi.wu@selecthealth.org>
2024-12-03 11:06:40 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AWD_Create_Helper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
